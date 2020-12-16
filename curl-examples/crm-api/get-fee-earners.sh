315afec25fcf54ed4da519725a8a885f33ee3783
Richard Salt <richard.salt@quill.co.uk>
2020-12-16 17:55:33 +0000

    Create get-fee-earners.sh

---
+++ curl-examples/crm-api/get-fee-earners.sh
@@ -0,0 +1,4 @@
+curl --location --request GET 'https://api.{quill-domain}/quillapi5/feeearners' \
+ --header 'clientId: {tenant-id}' \
+ --header 'Accept: application/Json' \
+ --header 'Authorization: Bearer {your-token}'
