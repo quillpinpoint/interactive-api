0cdb5b5e86e49df9295f5bc21e0c7f734a1f38fe
Richard Salt <richard.salt@quill.co.uk>
2021-03-24 18:41:20 +0000

    Create clients-changed.sh

---
+++ curl-examples/crm-api/clients-changed.sh
@@ -0,0 +1,4 @@
+curl --location --request GET 'https://api.{{quill-domain}}/quillapi5/clients-changed?since=2020-09-01T00:00:00Z' \
+--header 'clientId: {tenantId}' \
+--header 'Authorization: Bearer {token}'
+
