5b448d1f96a6ef34a5a5fab0c9501461dafb6b7d
Richard Salt <richard.salt@quill.co.uk>
2021-05-12 11:52:29 +0100

    Update create-case.sh

--- curl-examples/crm-api/create-case.sh
+++ curl-examples/crm-api/create-case.sh
@@ -4,5 +4,12 @@ curl --location --request POST 'https://api.ourdomain.co.uk/quillapi5/cases' \
 --header 'Authorization: Bearer {your-token}' \
 --header 'Content-Type: application/json' \
 --data-raw '{
-TODO
+  "VendorCode": "YOUR-CASE-REF",
+  "Description": "Description of this case",
+  "DateOpened": "2020-05-04T00:00:00+01:00",
+  "CaseType": { "Code": "01" },
+  "Branch": { "Code": "1" },
+  "FeeEarner": { "Code": "3" },
+  "Partner": { "Code": "3" },
+  "Source": { "Code": "4" }
 }'
