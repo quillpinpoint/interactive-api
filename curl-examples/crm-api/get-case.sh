5fcf39a12116cc8e8b2fa5126be3eeab42406c12
Richard Salt <richard.salt@quill.co.uk>
2021-06-17 13:47:27 +0100

    Update get-case.sh

--- curl-examples/crm-api/get-case.sh
+++ curl-examples/crm-api/get-case.sh
@@ -1,4 +1,4 @@
-#based on standard quill case code
+#based on the standard Quill case code (aka "DisplayCode")
 curl --location --request GET 'https://{domain}/quillapi5/cases/CRU0011' \
 --header 'clientId: {tenantId}' \
 --header 'Authorization: Bearer {token}'
