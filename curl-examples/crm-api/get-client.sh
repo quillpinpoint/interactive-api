ed4e808485886afe1814aa35b69eef3db105ef3a
Richard Salt <richard.salt@quill.co.uk>
2021-10-01 17:59:17 +0100

    Update get-client.sh

--- curl-examples/crm-api/get-client.sh
+++ curl-examples/crm-api/get-client.sh
@@ -1,4 +1,4 @@
 #you must always specify fetchChildCollections whether true or false
-curl --location --request GET 'https://api.{our-domain}.co.uk/quillapi5/clients/CRU001?fetchChildCollections=false' \
+curl --location --request GET 'https://api.{quill-domain}.co.uk/quillapi5/clients/CRU001?fetchChildCollections=false' \
 --header 'clientId: {tenantId}' \
 --header 'Authorization: Bearer {token}'
