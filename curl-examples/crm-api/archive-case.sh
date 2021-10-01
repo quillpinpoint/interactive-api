97134c7922af06135402db3a204d4373ca6b85ac
Richard Salt <richard.salt@quill.co.uk>
2021-10-01 17:57:24 +0100

    Update archive-case.sh

--- curl-examples/crm-api/archive-case.sh
+++ curl-examples/crm-api/archive-case.sh
@@ -1,4 +1,4 @@
-curl --location --request POST 'https://api.{our-domain}.co.uk/quillapi5/archive \
+curl --location --request POST 'https://api.{quill-domain}.co.uk/quillapi5/archive \
 --header 'clientId: {your-tenant-id-here}' \
 --header 'Accept: application/Json' \
 --header 'Authorization: Bearer {your-token}' \
