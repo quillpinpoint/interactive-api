b526c6355ea65d8d34511f8ab968cd7e16ca0d2d
Richard Salt <richard.salt@quill.co.uk>
2021-10-01 17:57:37 +0100

    Update cases-changed.sh

--- curl-examples/crm-api/cases-changed.sh
+++ curl-examples/crm-api/cases-changed.sh
@@ -1,10 +1,10 @@
 # with +1 hour offset e.g. during British Summer Time (BST) - note the + is urlencoded
-curl --location --request GET 'https://api.{our-domain}.co.uk/quillapi5/cases-changed?since=2021-01-01T00:00:00%2B1:00' \
+curl --location --request GET 'https://api.{quill-domain}.co.uk/quillapi5/cases-changed?since=2021-01-01T00:00:00%2B1:00' \
 --header 'clientId: {tenantId}' \
 --header 'Authorization: Bearer {token}'
 
 # no offset for regular GMT
-curl --location --request GET 'https://api.{our-domain}/quillapi5/cases-changed?since=2021-01-01T00:00:00Z' \
+curl --location --request GET 'https://api.{quill-domain}/quillapi5/cases-changed?since=2021-01-01T00:00:00Z' \
 --header 'clientId: {tenantId}' \
 --header 'Authorization: Bearer {token}'
 
